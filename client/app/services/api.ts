ace5488bf89ba1ca5a5caed5b3a6f0d181712541
adilelli <adil.elli09@gmail.com>
2026-03-26 17:52:15 +0800

    add api

---
+++ client/app/services/api.ts
@@ -0,0 +1,23 @@
+export const API_BASE = import.meta.env.VITE_API_BASE;
+
+export async function apiFetch<T>(
+  endpoint: string,
+  options: RequestInit = {}
+): Promise<T> {
+    console.log(API_BASE);
+  const url = `${API_BASE}${endpoint}`;
+  const response = await fetch(url, {
+    headers: {
+      'Content-Type': 'application/json',
+      ...options.headers,
+    },
+    ...options,
+  });
+
+  if (!response.ok) {
+    const errorText = await response.text();
+    throw new Error(`API Error ${response.status}: ${errorText}`);
+  }
+
+  return response.status === 204 ? (null as T) : await response.json();
+}
