cc4d98c8bee7c2464f5b14d3eadbd791b02fcd9c
adilelli <adil.elli09@gmail.com>
2026-03-27 01:12:31 +0800

    add security for token handling

--- server/app/core/security.py
+++ server/app/core/security.py
@@ -1,20 +1,31 @@
-from datetime import datetime, timedelta
-from jose import jwt
-from passlib.context import CryptContext
+from jose import JWTError, jwt
 from app.core.config import settings
+from pwdlib import PasswordHash
+import datetime as dt
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+password_hash = PasswordHash.recommended()
 
-def hash_password(password: str):
-    return pwd_context.hash(password)
+def hash_password(password: str) -> str:
+    return password_hash.hash(password)
 
 def verify_password(plain, hashed):
-    return pwd_context.verify(plain, hashed)
+    return password_hash.verify(plain, hashed)
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.now(datetime.timezone.utc) + timedelta(
+    expire = dt.datetime.now(dt.timezone.utc) + dt.timedelta(
         minutes=settings.access_token_expire_minutes
     )
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, settings.secret_key, algorithm=settings.algorithm)
+
+def verify_token(token: str) -> dict:
+    try:
+        payload = jwt.decode(
+            token,
+            settings.secret_key,
+            algorithms=[settings.algorithm]
+        )
+        return payload
+    except JWTError:
+        raise ValueError("Invalid or expired token")
