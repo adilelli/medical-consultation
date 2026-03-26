57ebdfa83b6debae824a8401c3735d8cb88ae034
adilelli <adil.elli09@gmail.com>
2026-03-27 01:14:40 +0800

    update exception handling

--- server/app/services/consultation_service.py
+++ server/app/services/consultation_service.py
@@ -2,6 +2,7 @@ from app.repositories.consultation_repository import ConsultationRepository
 from app.repositories.diagnosis_repository import DiagnosisRepository
 from app.repositories.user_repository import UserRepository
 from app.schemas.consultation_schema import ConsultationCreate
+from app.core.exceptions import UnauthorizedError, ForbiddenError, BusinessLogicError
 
 class ConsultationService:
     def __init__(self, repo: ConsultationRepository, user: UserRepository, diagnosis: DiagnosisRepository):
@@ -12,15 +13,15 @@ class ConsultationService:
     async def create_consultation(self, consultation: ConsultationCreate):
         doctor = await self.user.get_user_by_id(consultation.doctor_id)
         if doctor is None:
-            raise ValueError("Doctor ID does not exist")
+            raise BusinessLogicError("Doctor ID does not exist")
         
         patient = await self.user.get_user_by_id(consultation.patient_id)
         if patient is None:
-            raise ValueError("Patient ID does not exist")
+            raise BusinessLogicError("Patient ID does not exist")
         
         diagnosis = await self.diagnosis.get_diagnosis_by_id(consultation.diagnosis_id)
         if diagnosis is None:
-            raise ValueError("Diagnosis ID does not exist")
+            raise BusinessLogicError("Diagnosis ID does not exist")
 
         db_consultation = await self.repo.create_consultation(consultation)
 
